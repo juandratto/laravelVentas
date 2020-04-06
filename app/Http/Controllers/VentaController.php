d97b3be6a8b55d0ab180a6ee4d6487fb93f76f44
juand.ratto <juand.ratto@gmail.com>
2020-04-06 10:00:13 -0500

    venta controller quitar cod fact elect

--- app/Http/Controllers/VentaController.php
+++ app/Http/Controllers/VentaController.php
@@ -17,17 +17,6 @@ use Response;
 use Illuminate\Support\Collention;
 use DateTime;
 
-// Librerias Greenter - Generacion de factura electronica Sunat
-use Greenter\Model\Sale\Invoice;
-use Greenter\Model\Sale\SaleDetail;
-use Greenter\Model\Sale\Legend;
-use Greenter\Ws\Services\SunatEndpoints;
-use Greenter\Model\Client\Client;
-use Greenter\Model\Company\Company;
-use Greenter\Model\Company\Address;
-
-//require __DIR__ . 'sisVentas\..\vendor\autoload.php';
-
 class VentaController extends Controller
 {
       public function __construct()
@@ -91,84 +80,7 @@ class VentaController extends Controller
     		$descuento = $request->get('descuento');
     		$precio_venta = $request->get('precio_venta');
 
-			//$util = Util::getInstance();
-			// Genera factura??
-			$client = new Client();
-			$client->setTipoDoc('6')
-			->setNumDoc('20000000001')
-			->setRznSocial('EMPRESA 1');
-			
-			// Emisor
-			$address = new Address();
-			$address->setUbigueo('150101')
-			->setDepartamento('LIMA')
-			->setProvincia('LIMA')
-			->setDistrito('LIMA')
-			->setUrbanizacion('NONE')
-			->setDireccion('AV LS');
-			
-			$company = new Company();
-			$company->setRuc('20000000001')
-			->setRazonSocial('EMPRESA SAC')
-			->setNombreComercial('EMPRESA')
-			->setAddress($address);
-			
-
-			$invoice = (new Invoice());
-			$invoice->setUblVersion('2.1')
-			->setTipoOperacion('0101') // Catalog. 51
-			->setTipoDoc('01')
-			->setSerie('F001')
-			->setCorrelativo('4')
-			->setFechaEmision(new DateTime())
-			->setTipoMoneda('PEN')
-			->setClient($client)
-			->setMtoOperGravadas(100.00)
-			->setMtoIGV(18.00)
-			->setTotalImpuestos(18.00)
-			->setValorVenta(100.00)
-			->setSubTotal(118.00)
-			->setMtoImpVenta(118.00)
-			->setCompany($company);
-
 			$cont = 0;
-			//items de la factura
-			$ceItems = [];
-			$item = (new SaleDetail())
-			->setCodProducto('P001')
-			->setUnidad('NIU')
-			->setCantidad(2)
-			->setDescripcion('PRODUCTO 1')
-			->setMtoBaseIgv(100)
-			->setPorcentajeIgv(18.00) // 18%
-			->setIgv(18.00)
-			->setTipAfeIgv('10')
-			->setTotalImpuestos(18.00)
-			->setMtoValorVenta(100)
-			->setMtoValorUnitario(50.00)
-			->setMtoPrecioUnitario(59.00);
-
-			$legend = (new Legend())
-			->setCode('1000')
-			->setValue('SON DOSCIENTOS TREINTA Y SEIS CON 00/100 SOLES');
-
-			$invoice->setDetails([$item])
-					->setLegends([$legend]);
-
-			$see = require __DIR__.'/../../../config.php';
-			$result = $see->send($invoice);
-
-			// Guardar XML
-			file_put_contents($invoice->getName().'.xml',
-							$see->getFactory()->getLastXml());
-			if (!$result->isSuccess()) {
-				var_dump($result->getError());
-				exit();
-			}
-
-			echo $result->getCdrResponse()->getDescription();
-			// Guardar CDR
-			file_put_contents('R-'.$invoice->getName().'.zip', $result->getCdrZip());
 
     		while($cont < count($idproducto)){
 
